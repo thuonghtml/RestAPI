8377689d1eeccc208fa35f2c1dbefe9197ab5f42
DESKTOP-U1GLCMN\NguyenMinhThuong <thuonghtml@gmail.com>
2017-11-16 12:58:35 +0700

    Initial commit

---
+++ app/Http/Controllers/RegistrationController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class RegistrationController extends Controller
+{
+    public function store(Request $request)
+    {
+        //
+    }
+
+    public function destroy($id)
+    {
+        //
+    }
+}
