a1187438698c8089917fecd8def6ce5b449339f6
DESKTOP-U1GLCMN\NguyenMinhThuong <thuonghtml@gmail.com>
2017-11-16 13:18:14 +0700

    API to create a meeting

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,6 +3,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\User;
 use Validator;
+use Tymon\JWTAuth\Exceptions\JWTException
 use JWTAuth;
 class AuthController extends Controller
 {
--- app/Http/Controllers/MeetingController.php
+++ app/Http/Controllers/MeetingController.php
@@ -1,19 +1,98 @@
 <?php
-
 namespace App\Http\Controllers;
-
 use Illuminate\Http\Request;
-
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use App\Meeting;
+use JWTAuth;
+use Carbon\Carbon;
 class MeetingController extends Controller
 {
-   public function create()
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function __construct()
+    {
+        $this->middleware('jwt.auth', ['only' => [
+            'store', 'update', 'destroy'
+        ]]);
+    }
+    public function index()
     {
         //
     }
-   public function edit($id)
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'title' => 'required',
+            'description' => 'required',
+            'time' => 'required|date_format:YmdHie'
+        ]);
+        if(!$user = JWTAuth::parseToken()->authenticate())
+        {
+            return response()->json(['msg' => "User not found", 404]);
+        }
+        $title = $request->input('title');
+        $description = $request->input('description');
+        $time = $request->input('time');
+        $user_id = $user->id;
+        $meeting = new Meeting([
+            'time' => Carbon::createFromFormat('YmdHie', $time),
+            'title' => $title,
+            'description' => $description
+        ]);
+        if ($meeting->save()) {
+            $meeting->users()->attach($user_id);//should check if user_id is exist in users table
+            $meeting->view_meeting = [
+                'href' => 'api/v1/meeting/' . $meeting->id,
+                'method' => 'GET'
+            ];
+            $message = [
+                'msg' => 'Meeting created',
+                'meeting' => $meeting
+            ];
+            return response()->json($message, 201);
+        }
+        $response = [
+            'msg' => 'Error during creation'
+        ];
+        return response()->json($response, 404);
+    }
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+    }
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
     {
         //
     }
-
-
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
 }
